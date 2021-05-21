d126761cb30598aea0ea5a5fdb5529c87f0d64b2
Alicia Weenum <weenum.alicia@gmail.com>
2021-05-20 22:58:29 -0400

    seed data adds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,96 @@ Part.create([
     {name: "TERMINAL BOARD ASSEMBLY", niin: "45", inc: "08573", fsc: "5940", publication_date: "AUG 2013"},
     {name: "MODIFICATION KIT,AIRFRAME", niin: "47", inc: "39730", fsc: "1560", publication_date: "AUG 2013"},
     {name: "ENGINE,AIRCRAFT,TURBO-SHAFT", niin: "48", inc: "32600", fsc: "2840", publication_date: "AUG 2013"},
-    {name: "MODIFICATION KIT,ENGINE",  niin: "49", inc: "39733", fsc: "2840", publication_date: "AUG 2013"}
+    {name: "MODIFICATION KIT,ENGINE",  niin: "49", inc: "39733", fsc: "2840", publication_date: "AUG 2013"},
+    {niin: "47",name: "MODIFICATION KIT,AIRFRAME", inc: "39730", fsc: "1560", publication_date: "AUG 2013"},
+    {niin: "48",
+    name: "ENGINE,AIRCRAFT,TURBO-SHAFT",
+    inc: "32600",
+    fsc: "2840",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "49",
+    name: "MODIFICATION KIT,ENGINE",
+    inc: "39733",
+    fsc: "2840",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "50",
+    name: "COMPRESSOR UNIT,RECIPROCATING",
+    inc: "29301",
+    fsc: "4310",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "51",
+    name: "ELECTRON TUBE",
+    inc: "00001",
+    fsc: "5960",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "57",
+    name: "CLOSER,DOOR",
+    inc: "06689",
+    fsc: "5340",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "58",
+    name: "CLOSER,DOOR",
+    inc: "06689",
+    fsc: "5340",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "59",
+    name: "CLOSER,DOOR",
+    inc: "06689",
+    fsc: "5340",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "60",
+    name: "CLOSER,DOOR",
+    inc: "06689",
+    fsc: "5340",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "61",
+    name: "MACHINE GUN,7.62 MILLIMETER",
+    inc: "21601",
+    fsc: "1005",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "64",
+    name: "BUR,DENTAL,EXCAVATING",
+    inc: "11393",
+    fsc: "6520",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "65",
+    name: "SHIPPING AND STORAGE",
+    inc: "77777",
+    fsc: "8140",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "67",
+    name: "CLOTH,TWILL",
+    inc: "29987",
+    fsc: "8305",
+    publication_date: "AUG 2013"
+},
+{
+    niin: "68",
+    name: "COVER,ACCESS",
+    inc: "29785",
+    fsc: "5340",
+    publication_date: "AUG 2013"
+},
 ])
